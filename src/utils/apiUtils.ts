3d7784dddca40c2201716de207da5c217f07d3a5
woojiahao <woojiahao1234@gmail.com>
2020-05-05 14:53:38 +0800

    Add utility for API calls

---
+++ src/utils/apiUtils.ts
@@ -0,0 +1,21 @@
+const API_URL = process.env.VUE_APP_API_URL
+
+interface UrlArgs {
+  [Key: string]: string | number;
+}
+
+export const call = async (endpoint: string, args: UrlArgs = {}): Promise<object> => {
+  const url = constructUrl(endpoint, args)
+  console.log(`querying ${url}`)
+  const data = await fetch(url)
+  console.log(data)
+  return data
+}
+
+const constructUrl = (endpoint: string, args: UrlArgs): string => {
+  const url = new URL(endpoint, API_URL)
+  Object.entries(args).forEach(value => {
+    url.searchParams.set(value[0], value[1].toString())
+  })
+  return url.toString()
+}
